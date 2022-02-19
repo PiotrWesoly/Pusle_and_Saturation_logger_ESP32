5b18fd182dc30537aa7a1556ab48b002b7290c7c
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-02-19 15:11:57 +0100

    Almost finished, gotta change listener
    Almost finished ESP part. It is storing HR, spO2 and time (although only fo r 3 hrs). It is sending the history in the beggining and then current readings

--- Main/Main.ino
+++ Main/Main.ino
@@ -5,17 +5,13 @@
 #include "MAX30105.h"
 #include "spo2_algorithm.h"
 
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
+#include "BluetoothSerial.h"
 #include <string>
 
 using namespace std;
 /******************************************************************************************************
  *                                       Defines                                                      *
  *****************************************************************************************************/
-#define SERVICE_UUID        BLEUUID((uint16_t)0x1822)
-#define CHARACTERISTIC_UUID BLEUUID((uint16_t)0x2A5F) 
 #define BUFFER_LENGTH       11100
 #define MAX_BRIGHTNESS      255
 
@@ -26,35 +22,20 @@ typedef struct Readings
 {
   uint8_t heartRate;
   uint8_t spO2;
-  uint32_t time;
+  uint32_t sampleTime;
 } Readings_struct;
 
 /******************************************************************************************************
  *                                      Global variables                                              *
  *****************************************************************************************************/
+/********** Bluetooth object ************/
+BluetoothSerial SerialBT;
 
-/********** BLE variables ************/
-bool deviceConnected = false;
-BLECharacteristic readCharacteristic(
-  BLEUUID((uint16_t)0x1004), 
-  BLECharacteristic::PROPERTY_READ| 
-  BLECharacteristic::PROPERTY_NOTIFY
-);
-
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
-};
 /********** Sensor variables ************/
 uint16_t bufferIndex=0;
-bool connectedFirst=true;
+bool firstConnection=true;
 
-//Buffer allocation
+/* Buffer allocation */
 Readings_struct readings[BUFFER_LENGTH];
 //Readings_struct* readings = (Readings_struct*) malloc(150000 * sizeof(Readings_struct));
 
@@ -74,33 +55,10 @@ int8_t validHeartRate; //indicator to show if the heart rate calculation is vali
  *                                      Setup                                                          *
  *****************************************************************************************************/
 void setup() {
-  /********************* BLE setput ***********************/
+  /************************* Initialize bluetooth ***********************/
+  SerialBT.begin("Pulse and saturation logger");
   Serial.begin(115200);
-  Serial.print("Starting BLE work!");
-
-  // Create the BLE Device
-  BLEDevice::init("Pulse and spO2 logger");
-
-  // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  // Create a BLE Characteristic
-  pService->addCharacteristic(&readCharacteristic);
-
-  pServer->getAdvertising()->addServiceUUID(SERVICE_UUID);
-
-  // Start the service
-  pService->start();
-
-  // Start advertising
-  pServer->getAdvertising()->start();
-
-    Serial.print("Finished bel work!");
-
+  
   /************************* Initialize sensor ***********************/
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
@@ -109,7 +67,7 @@ void setup() {
   }
 
   Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-  while (Serial.available() == 0) ; //wait until user presses a key
+  while (Serial.available() == 0) ; /* wait until user presses a key */
   Serial.read();
 
   /********************************   Setup configurations **********************************/
@@ -128,14 +86,14 @@ void setup() {
  *****************************************************************************************************/
 void loop()
 {
-  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+  bufferLength = 100; /* buffer length of 100 stores 4 seconds of samples running at 25sps */
 
   /***************** Read frist 100 samples for range estimation *********************/
-  //read the first 100 samples, and determine the signal range
+  /* read the first 100 samples, and determine the signal range */
   for (byte i = 0 ; i < bufferLength ; i++)
   {
-    while (particleSensor.available() == false) //do we have new data?
-      particleSensor.check(); //Check the sensor for new data
+    while (particleSensor.available() == false) /* do we have new data? */
+      particleSensor.check(); /* Check the sensor for new data */
 
     redBuffer[i] = particleSensor.getRed();
     irBuffer[i] = particleSensor.getIR();
@@ -147,21 +105,21 @@ void loop()
     Serial.println(irBuffer[i], DEC);
   }
 
-  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  /* Calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples) */
   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
   /***************** Continously read new samples (25/reading) *********************/
-  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  /* Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second */
   while (1)
   {
-    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    /* dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top */
     for (byte i = 25; i < 100; i++)
     {
       redBuffer[i - 25] = redBuffer[i];
       irBuffer[i - 25] = irBuffer[i];
     }
 
-    //take 25 sets of samples before calculating the heart rate.
+    /* take 25 sets of samples before calculating the heart rate. */
     for (byte i = 75; i < 100; i++)
     {
       while (particleSensor.available() == false) //do we have new data?
@@ -172,7 +130,7 @@ void loop()
       particleSensor.nextSample(); //We're finished with this sample so move to next sample
     }
 
-    //After gathering 25 new samples recalculate HR and SP02
+    /* After gathering 25 new samples recalculate HR and SP02 */
      maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
       /****************************  Save readings to the buffer ***************************/
@@ -184,7 +142,7 @@ void loop()
      Serial.println(spo2, DEC);
      readings[bufferIndex].heartRate = (uint8_t) heartRate;
      readings[bufferIndex].spO2 = (uint8_t) spo2;
-     readings[bufferIndex].time = millis();
+     readings[bufferIndex].sampleTime = millis();
      }
      else
      {
@@ -192,52 +150,34 @@ void loop()
      }
 
      /**************************** When connected to phone ********************************/
-    if(deviceConnected)
-    {
-      if(connectedFirst == true)
+     Serial.println(SerialBT.available());
+     Serial.println(bufferIndex);
+      if(SerialBT.available())
       {
-       for(uint32_t i =0; i<bufferIndex; i++)
-       {
-//         string heartRateMsg((char*)&readings[i].heartRate, 1);
-//         string spO2Msg((char*)&readings[i].spO2, 1);
-//         string timeMsg((char*)&readings[i].time, 4);
-//         Serial.print(F(", BufferIndex="));
-//         Serial.println(i, DEC);
-//         Serial.print(F(", BT="));
-//         Serial.println(readings[i].heartRate, DEC);
+        if(firstConnection == true)   /* On first connection send the history of readings */
+        {
+          for(uint32_t i =0; i<=bufferIndex; i++)
+          {       
+            /* Print buffer to serial monitor */
+            Serial.print("Buffer: ");
+            Serial.println(readings[i].heartRate, DEC);
 
-          
-
-//          Msg = (string)readings[i].heartRate+','+(string)readings[i].spO2+','+(string)readings[i].time;
-//          readCharacteristic.setValue(heartRateMsg);
-//          readCharacteristic.setValue(",");
-//          readCharacteristic.setValue(spO2Msg);
-//          readCharacteristic.setValue(",");
-//          readCharacteristic.setValue(timeMsg);
-//          readCharacteristic.setValue(";");
-
-          Serial.print(readings[i].heartRate, DEC);
-          Serial.print(F(","));
-          Serial.print(readings[i].spO2, DEC);
-          Serial.print(F(","));
-          Serial.print(readings[i].time, DEC);
-          Serial.println(F(";"));
-          
-         
-         readCharacteristic.setValue( (uint8_t*)&readings[i].heartRate, 1);
-         readCharacteristic.setValue( (uint8_t*)&readings[i].spO2, 1);
-         readCharacteristic.setValue( (uint8_t*)&readings[i].time, 4);
-       }
-       connectedFirst = false;
+            /* Send previous readings */
+            SerialBT.write(readings[i].heartRate);
+            SerialBT.write(readings[i].spO2);
+            SerialBT.write(readings[i].sampleTime);
+            
+          }
+          firstConnection = false;
+        }
+        else                          /* After receiving all history keep sending current reading */
+        {
+          SerialBT.write(readings[bufferIndex].heartRate);
+          SerialBT.write(readings[bufferIndex].spO2);
+          SerialBT.write(readings[bufferIndex].sampleTime);
+        }
       }
-      else
-      {
-        readCharacteristic.setValue( (uint8_t*)&readings[bufferIndex].heartRate, 1);
-        readCharacteristic.setValue( (uint8_t*)&readings[i].spO2, 1);
-        readCharacteristic.setValue( (uint8_t*)&readings[i].time, 4);
-      }
-    }
-     bufferIndex++;
-     if(bufferIndex >= BUFFER_LENGTH) bufferIndex = 0;
+      bufferIndex++;
+      if(bufferIndex >= BUFFER_LENGTH) bufferIndex = 0;
   }
 }

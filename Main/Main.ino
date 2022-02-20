01cde8422c5e0889aa7e5180f9c00a106c07a8a4
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-02-20 17:44:26 +0100

    prevent double sending, send sample time offset, buffer roll over handler
    I added isSend array, which tells which sample was already sent. Also I added algorythm to send sample time offset, which then gets calculated in the phone. I also added a buffer roll over handler which saves not sent samples in buffer so that it will get send first

--- Main/Main.ino
+++ Main/Main.ino
@@ -11,10 +11,12 @@ using namespace std;
 /******************************************************************************************************
  *                                       Defines                                                      *
  *****************************************************************************************************/
-#define BUFFER_LENGTH         11300
+#define BUFFER_LENGTH         7000 //11300
 #define MAX_BRIGHTNESS        255
 #define CURRENT_READING_MSG   1
 #define HISTORY_MSG           2
+#define SENT                  1
+#define NOT_SENT              0
 
 /******************************************************************************************************
  *                                      Typedefs                                                      *
@@ -69,7 +71,7 @@ void setup() {
   }
 
   Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-  while (Serial.available() == 0) ; /* wait until user presses a key */
+  while (Serial.available() == 0) ; /* wait until user starts */
   Serial.read();
 
   /********************************   Setup configurations **********************************/
@@ -88,8 +90,10 @@ void setup() {
  *****************************************************************************************************/
 void loop()
 {
+  static int isSent[BUFFER_LENGTH] = {0};
+  
   /* Wait for the user to start the sensor */
-  while(!SerialBT.available()){}
+//  while(!SerialBT.available()){}
   bufferLength = 100; /* buffer length of 100 stores 4 seconds of samples running at 25sps */
 
   /***************** Read frist 100 samples for range estimation *********************/
@@ -116,6 +120,8 @@ void loop()
   /* Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second */
   while (1)
   {
+    uint32_t startTime = 0;
+    uint32_t timeOfRecord = 0;
     /* dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top */
     for (byte i = 25; i < 100; i++)
     {
@@ -127,7 +133,7 @@ void loop()
     for (byte i = 75; i < 100; i++)
     {
       while (particleSensor.available() == false) //do we have new data?
-        particleSensor.check(); //Check the sensor for new data
+      particleSensor.check(); //Check the sensor for new data
 
       redBuffer[i] = particleSensor.getRed();
       irBuffer[i] = particleSensor.getIR();
@@ -140,13 +146,20 @@ void loop()
     /****************************  Save readings to the buffer ***************************/
     if(heartRate != -999)
     {
-      Serial.print(F(", HR="));
-      Serial.print(heartRate, DEC);
-      Serial.print(F(", spo2="));
-      Serial.println(spo2, DEC);
-      readings[bufferIndex].heartRate = (uint8_t) heartRate;
-      readings[bufferIndex].spO2 = (uint8_t) spo2;
-      readings[bufferIndex].sampleTime = millis();
+        if(bufferIndex == 0)
+        {
+          startTime = millis();
+        }
+        Serial.print(F(", HR="));
+        Serial.print(heartRate, DEC);
+        Serial.print(F(", spo2="));
+        Serial.println(spo2, DEC);
+        Serial.print(F(", index="));
+        Serial.println(bufferIndex, DEC);
+        readings[bufferIndex].heartRate = (uint8_t) heartRate;
+        readings[bufferIndex].spO2 = (uint8_t) spo2;
+        readings[bufferIndex].sampleTime = millis() - startTime;
+
     }
     else
     {
@@ -160,33 +173,46 @@ void loop()
     {
       uint8_t* pointer;
       uint8_t converter[4];
+      uint32_t currentMillis = millis();
       if(firstConnection == true)   /* On first connection send the history of readings */
       {
         for(uint32_t i =0; i<=bufferIndex; i++)
-        {       
-          pointer = (uint8_t*)&readings[i].sampleTime;
-          /* Endiannes swap */
-          converter[0] = pointer[3];
-          converter[1] = pointer[2];
-          converter[2] = pointer[1];
-          converter[3] = pointer[0];
-          /* Print buffer to serial monitor */
-          Serial.print("Buffer: ");
-          Serial.println(readings[i].heartRate, DEC);
+        {
+          if(isSent[i] == NOT_SENT)
+          {
+            timeOfRecord = currentMillis - readings[i].sampleTime;
+            Serial.print("currentMillis: ");
+            Serial.println(currentMillis);
+           Serial.print("sampletime: ");
+            Serial.println(readings[i].sampleTime);
+           Serial.print("curent-sample: ");
+            Serial.println(timeOfRecord);
+            
+            pointer = (uint8_t*)&timeOfRecord;
+            /* Endiannes swap */
+            converter[0] = pointer[3];
+            converter[1] = pointer[2];
+            converter[2] = pointer[1];
+            converter[3] = pointer[0];
+            /* Print buffer to serial monitor */
+//            Serial.print("Buffer: ");
+//            Serial.println(readings[i].heartRate, DEC);
+      
+            /* Send previous readings */
+            SerialBT.write(readings[i].heartRate);
+            SerialBT.write(readings[i].spO2);
+            SerialBT.write(converter[0]);
+            SerialBT.write(converter[1]);
+            SerialBT.write(converter[2]);
+            SerialBT.write(converter[3]);
+            SerialBT.write((uint8_t)HISTORY_MSG);
     
-          /* Send previous readings */
-          SerialBT.write(readings[i].heartRate);
-          SerialBT.write(readings[i].spO2);
-          SerialBT.write(converter[0]);
-          SerialBT.write(converter[1]);
-          SerialBT.write(converter[2]);
-          SerialBT.write(converter[3]);
-          SerialBT.write((uint8_t)HISTORY_MSG);
-          
+            isSent[i] = SENT;
+          }
         }
         firstConnection = false;
       }
-      else                          /* After receiving all history keep sending current reading */
+      else /* After receiving all history keep sending current reading */
       {
         /* uint32 to uint8[4] converter */
         pointer = (uint8_t*)&readings[bufferIndex].sampleTime;
@@ -205,9 +231,28 @@ void loop()
         SerialBT.write(converter[3]);
         SerialBT.write((uint8_t)CURRENT_READING_MSG);
         Serial.println(readings[bufferIndex].sampleTime, DEC);
+        isSent[bufferIndex] = SENT;
+        
       }
     }
-    bufferIndex++;
-    if(bufferIndex >= BUFFER_LENGTH) bufferIndex = 0;
+    
+    bufferIndex++;/* increment index */
+
+    /* If buffer rolled over */
+    if(bufferIndex >= BUFFER_LENGTH) 
+    {
+      bufferIndex = 0;
+      for(uint32_t i=0; i<=BUFFER_LENGTH; i++)
+      {
+        if(isSent[i] == NOT_SENT)
+        {
+          readings[bufferIndex].heartRate = readings[i].heartRate;
+          readings[bufferIndex].spO2 = readings[i].spO2;
+          readings[bufferIndex].sampleTime = readings[i].sampleTime;
+          bufferIndex++;
+        }
+      }
+      for(int i=0; i<=BUFFER_LENGTH;i++) isSent[i]=NOT_SENT;
+    }
   }
 }
